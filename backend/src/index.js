35ad95d498434c39677fb46ce9a8b7c1e47d145a
Joevinoth <jj.vinoth@outlook.com>
2025-04-07 22:05:57 +0530

    Initial config done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,14 @@
 import express from "express";
-import "dotenv/config";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
-// const PORT = process.env.PORT;
+const PORT = process.env.PORT;
 
-const PORT = 3000;
-
-console.log(PORT);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
